b2992328dfb9c0a306bf10d8d79d6c99616d5eef
Aditya Sharma <nightthrasher10@gmail.com>
2023-04-10 03:38:21 +0530

    updated user model

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	username: {
+		type: String,
+		required: false,
+	},
 	picture: {
 		type: String,
 		required: true,
